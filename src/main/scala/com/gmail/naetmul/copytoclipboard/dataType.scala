234bf19f834543121f501fca055841c606fa1200
rhlkmth <rahulkamath272@gmail.com>
2024-03-14 20:56:59 +0530

    Update dataType.scala

--- src/main/scala/com/gmail/naetmul/copytoclipboard/dataType.scala
+++ src/main/scala/com/gmail/naetmul/copytoclipboard/dataType.scala
@@ -2,6 +2,7 @@ package com.gmail.naetmul.copytoclipboard
 
 /**
  * Created by Haneul on 2014-08-01.
+ * Updated for Android 14 on 2023-03-14.
  */
 sealed trait DataType
 case object PlainTextType extends DataType
